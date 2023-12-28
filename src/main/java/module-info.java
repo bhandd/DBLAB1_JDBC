3234bb834b75df1ff194c72bf055d43beb9b9660
bjorn <bjorn1987@gmail.com>
2023-12-28 10:14:53 +0100

    added dialougeExample and updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module dblab1.dblab1_jdbc {
     requires javafx.fxml;
     requires java.sql;
 
-
+    exports dblab1.dblab1_jdbc.model.DialogExample;
     opens dblab1.dblab1_jdbc to javafx.fxml;
     exports dblab1.dblab1_jdbc;
 }
