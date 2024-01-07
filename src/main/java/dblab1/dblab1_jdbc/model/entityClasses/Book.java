755b76d31365f7476078e729a50a4bde388debd0
bjorn <bjorn1987@gmail.com>
2024-01-07 08:53:31 +0100

    added arraylist authors in Book

--- src/main/java/dblab1/dblab1_jdbc/model/entityClasses/Book.java
+++ src/main/java/dblab1/dblab1_jdbc/model/entityClasses/Book.java
@@ -1,6 +1,7 @@
 package dblab1.dblab1_jdbc.model.entityClasses;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
 /**
  * Representation of a book.
@@ -16,11 +17,17 @@ public class Book {
 
     private int year;
     private String storyLine = "";
+    private ArrayList<Author> authors;
     // TODO: 
     // Add authors, as a separate class(!), and corresponding methods, to your implementation
     // as well, i.e. "private ArrayList<Author> authors;"
-    
-    public Book(int bookId, String isbn, String title, int year/*Date published*/) {
+
+    //TODO: avkommentera arraylist med authors då detta krävs för att representera relationen mellan book och author
+    // avkommentera också String author som parameter i konstruktiorn
+    // när detta göra behöver man anpassa implementeringen i övriga programmet
+    public Book(int bookId, String isbn, String title, /*String authors*/ int year/*Date published*/) {
+      //  this.authors = new ArrayList<>();
+     //   this.authors.add(author)
         this.bookId = bookId;
         this.isbn = isbn;
         this.title = title;
