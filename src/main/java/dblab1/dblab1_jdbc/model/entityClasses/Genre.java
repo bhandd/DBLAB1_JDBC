00baaaf7721d147574f5e644fc028f1311aa5d88
Crazyicecat <noahjonsson35@gmail.com>
2024-01-09 14:19:27 +0100

    Added genre class

---
+++ src/main/java/dblab1/dblab1_jdbc/model/entityClasses/Genre.java
@@ -0,0 +1,30 @@
+package dblab1.dblab1_jdbc.model.entityClasses;
+
+public class Genre {
+
+    private int genreId;
+
+    private String name;
+
+    public Genre(int genreId, String name) {
+        this.genreId = genreId;
+        this.name = name;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "genreId=" + genreId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
